94808dc6a1b2458a48d514590d80d30316cf64cc
KimRass <rmx1000@yonsei.ac.kr>
2024-05-03 13:01:00 +0900

    Working on factorized self attn ViViT

--- vivit.py
+++ vivit.py
@@ -1,5 +1,6 @@
 # References:
     # https://github.com/huggingface/pytorch-image-models/blob/main/timm/models/vision_transformer.py
+    # https://github.com/rishikksh20/ViViT-pytorch/blob/master/vivit.py
 
 import einops
 import torch
@@ -15,8 +16,6 @@ from timm.models.vision_transformer import (
     LayerScale,
     DropPath,
 )
-
-# from vit import ViT
 
 
 """
@@ -41,14 +40,12 @@ class TubletEmbedding(nn.Module):
             tempo_patch_size, spat_patch_size, spat_patch_size,
         ) # "$t \times h \times h$"
         self.conv3d = nn.Conv3d(3, hidden_dim, kernel_size, stride, 0)
-        vit_patch_embed = vit.patch_embed
-        self.conv3d.bias.data = vit_patch_embed.proj.bias.data
-        self.to_seq = Rearrange("b l t h w -> b l t (h w)")
+        self.conv3d.bias.data = vit.patch_embed.proj.bias.data
+        self.to_seq = Rearrange("b d t h w -> b t (h w) d")
 
     def forward(self, x): # (B, C, T, H, W)
         x = self.conv3d(x) # (B, d, $n_{t}$, $n_{h}$, $n_{w}$)
-        x = self.to_seq(x)
-        return x.permute(0, 2, 3, 1)
+        return self.to_seq(x)
 
 
 class MSA(nn.Module):
@@ -89,25 +86,23 @@ class MSA(nn.Module):
         return x, attn_weight
 
 
-class ViViT(nn.Module):
-    def get_pos_embed_using_interpol(self, vit):
-        vit_img_size = vit.patch_embed.img_size
-        vit_patch_size = vit.patch_embed.patch_size
+class ViViTBase(nn.Module):
+    def get_pos_embed_using_interpol(self):
+        vit_img_size = self.vit.patch_embed.img_size
+        vit_patch_size = self.vit.patch_embed.patch_size
+        vit_pos_embed = self.vit.pos_embed
         vit_pos_embed_2d = vit_pos_embed[:, 1:, :].view(
             1,
             vit_img_size[0] // vit_patch_size[0],
             vit_img_size[1] // vit_patch_size[1],
             self.hidden_dim,
         )
-        pos_embed_2d = F.interpolate(
+        spat_pos_embed = F.interpolate(
             vit_pos_embed_2d.permute(0, 3, 1, 2),
-            size=img_size // spat_patch_size,
+            size=self.spat_seq_len,
         )
-        pos_embed_1d = einops.rearrange(
-            pos_embed_2d, pattern="1 d h w -> 1 (h w) d",
-        )
-        return torch.cat(
-            [vit_pos_embed[:, 0: 1, :], pos_embed_1d], dim=1,
+        return einops.rearrange(
+            spat_pos_embed, pattern="1 d h w -> 1 (h w) d",
         )
 
     def init_tempo_blocks(self):
@@ -124,95 +119,37 @@ class ViViT(nn.Module):
     def __init__(
         self,
         vit,
-        mode,
+        num_frames,
+        img_size,
+        tempo_patch_size,
+        spat_patch_size,
         pooling_mode="cls",
         num_classes=100,
-        img_size=224,
-        tempo_patch_size=2,
-        spat_patch_size=16,
-        num_tempo_layers=12,
-        num_spat_layers=12,
-        mlp_size=3072,
-        num_heads=12,
-        drop_prob=0.1,
     ):
         super().__init__()
 
-        self.mode = mode
+        self.vit = vit
         self.pooling_mode = pooling_mode
+        self.img_size = img_size
         self.hidden_dim = vit.embed_dim
+        self.spat_patch_size = spat_patch_size
 
-        self.tuplet_embed = TubletEmbedding(
+        self.tublet_embed = TubletEmbedding(
             vit=vit,
             tempo_patch_size=tempo_patch_size,
             spat_patch_size=spat_patch_size,
             hidden_dim=self.hidden_dim,
         )
-        self.cls_token = vit.cls_token
-        self.prepend_cls_token = lambda x: torch.cat(
-            (self.cls_token.repeat(x.size(0), 1, 1), x), dim=1,
-        )
-        self.pos_embed = self.get_pos_embed_using_interpol(vit=vit)
-        # x = torch.cat(
-        #     [einops.repeat(cls_token, pattern="d -> b 1 1 d", b=x.size(0)), x],
-        #     dim=1,
-        # )
+        self.merge_spat_tempo = Rearrange("b t s d -> b (t s) d")
+
+        self.spat_seq_len = img_size // spat_patch_size
+        self.tempo_seq_len = num_frames // tempo_patch_size
+        self.pos_embed = self.get_pos_embed_using_interpol()
+        self.vit_blocks = vit.blocks
         self.mlp_head = nn.Linear(self.hidden_dim, num_classes)
 
-        if self.mode == "spat_tempo_attn":
-            """
-            "Each transformer layer models all pairwise interactions between all
-            spatio-temporal tokens, and it thus models long-range interactions
-            across the video from the first layer."
-            """
-            self.merge = Rearrange("b t s d -> b (t s) d")
-            self.unmerge = Rearrange("b (t s) d -> b t s d")
-
-        if self.mode == "factor_encoder":
-            """
-            "The first, spatial encoder, only models interactions between tokens
-            extracted from the same temporal index."
-            "A representa- tion for each temporal index, hi ∈ Rd, is obtained after Ls layers: This is the encoded classification token, zcls Ls if it was prepended to the input (Eq. 1), or a global average pooling from the tokens output by the spatial encoder, zLs , otherwise."
-            "The frame-level representations, $h_{i}$, are concatenated into
-            $H \in \mathbb{R}^{n_{t} \times d}$, and then forwarded through a temporal encoder consisting of Lt transformer layers to model in- teractions between tokens from different temporal indices.
-            "The initial spatial encoder is identical to the one used for image classification."
-            """
-            self.fold_tempo = Rearrange("b t s d -> (b t) s d")
-            vit_blocks = vit.blocks
-            self.num_spat_layers = len(vit_blocks)
-            self.spat_encoder = vit_blocks
-            self.tempo_encoder = nn.Sequential(
-                *([vit_blocks[0]] * num_tempo_layers),
-            )
-
-        if self.mode == "factor_self_attn":
-            """
-            "We factorise the operation to first only compute self-attention spatially (among all tokens extracted from the same tem- poral index), and then temporally (among all tokens ex- tracted from the same spatial index)"
-            "We do not use a classification token in this model, to avoid
-            ambiguities when reshaping the input tokens between spatial and
-            temporal dimensions.
-            """
-            self.fold_tempo = Rearrange("b t s d -> (b t) s d")
-            self.fold_spat = Rearrange("b t s d -> (b s) t d")
-            vit_blocks = vit.blocks
-            self.spat_blocks = vit_blocks
-            self.tempo_blocks = nn.Sequential(
-                *(
-                    [
-                        MSA(
-                            hidden_dim=self.hidden_dim,
-                            num_heads=num_heads,
-                            drop_prob=drop_prob,
-                        ),
-                        nn.LayerNorm(self.hidden_dim),
-                    ] * len(vit_blocks)
-                )
-            )
-            self.init_tempo_blocks()
-            self.pooling_mode == "avg"
-
     @staticmethod
-    def unfold_tempo(x, batch_size):
+    def batch_to_tempo(x, batch_size):
         if x.ndim == 2:
             pattern = "(b t) d -> b t d"
         elif x.ndim == 3:
@@ -220,10 +157,7 @@ class ViViT(nn.Module):
         return einops.rearrange(x, pattern=pattern, b=batch_size)
 
     @staticmethod
-    def unfold_spat(x, batch_size):
-        # if x.ndim == 2:
-        #     pattern = "(b t) d -> b t d"
-        # elif x.ndim == 3:
+    def batch_to_spat(x, batch_size):
         pattern = "(b s) t d -> b s t d"
         return einops.rearrange(x, pattern=pattern, b=batch_size)
 
@@ -239,60 +173,234 @@ class ViViT(nn.Module):
             elif x.ndim == 4:
                 return torch.mean(x, dim=(1, 2))
 
+
+class SpatTempoAttnViViT(ViViTBase):
+    """
+    "Each transformer layer models all pairwise interactions between all
+    spatio-temporal tokens, and it thus models long-range interactions
+    across the video from the first layer."
+    """
+    def __init__(
+        self,
+        vit,
+        num_frames,
+        img_size,
+        tempo_patch_size,
+        spat_patch_size,
+        pooling_mode="cls",
+        num_classes=100,
+    ):
+        super().__init__(
+            vit=vit,
+            num_frames=num_frames,
+            img_size=img_size,
+            tempo_patch_size=tempo_patch_size,
+            spat_patch_size=spat_patch_size,
+            pooling_mode=pooling_mode,
+            num_classes=num_classes,
+        )
+
+        # self.cls_token = vit.cls_token
+        # self.cls_pos_embed = vit.pos_embed[:, 0: 1, :]
+        self.cls_token = nn.Parameter(torch.randn((1, 1, self.hidden_dim)))
+        self.cls_pos_embed = nn.Parameter(torch.randn((1, 1, self.hidden_dim)))
+        self.pos_embed = einops.repeat(
+            self.pos_embed, pattern="1 s d -> 1 t s d", t=self.tempo_seq_len,
+        )
+        self.prepend_cls_token = lambda x: torch.cat(
+            (self.cls_token.repeat(x.size(0), 1, 1), x), dim=1,
+        )
+        self.transformer = self.vit_blocks
+
+    def forward(self, x):
+        x = self.tublet_embed(x)
+        x = self.merge_spat_tempo(x)
+        x = self.prepend_cls_token(x)
+        x += torch.cat(
+            [self.cls_pos_embed, self.merge_spat_tempo(self.pos_embed)], dim=1,
+        )
+        x = self.transformer(x)
+        x = self.pool(x)
+        return self.mlp_head(x)
+
+
+class FactorEncViViT(ViViTBase):
+    """
+    "The first, spatial encoder, only models interactions between tokens
+    extracted from the same temporal index."
+    "A representa- tion for each temporal index, hi ∈ Rd, is obtained after Ls layers: This is the encoded classification token, zcls Ls if it was prepended to the input (Eq. 1), or a global average pooling from the tokens output by the spatial encoder, zLs , otherwise."
+    "The frame-level representations, $h_{i}$, are concatenated into
+    $H \in \mathbb{R}^{n_{t} \times d}$, and then forwarded through a temporal encoder consisting of Lt transformer layers to model in- teractions between tokens from different temporal indices.
+    "The initial spatial encoder is identical to the one used for image classification."
+    """
+    def __init__(
+        self,
+        vit,
+        num_frames,
+        img_size,
+        tempo_patch_size,
+        spat_patch_size,
+        pooling_mode="cls",
+        num_classes=100,
+    ):
+        super().__init__(
+            vit=vit,
+            num_frames=num_frames,
+            img_size=img_size,
+            tempo_patch_size=tempo_patch_size,
+            spat_patch_size=spat_patch_size,
+            pooling_mode=pooling_mode,
+            num_classes=num_classes,
+        )
+
+        self.spat_cls_token = nn.Parameter(
+            torch.randn((1, 1, self.hidden_dim)),
+        )
+        self.prepend_spat_cls_token = lambda x: torch.cat(
+            (self.spat_cls_token.repeat(x.size(0), 1, 1), x), dim=1,
+        )
+        self.spat_pos_embed = self.pos_embed
+        self.spat_cls_pos_embed = nn.Parameter(
+            torch.randn((1, 1, self.hidden_dim)),
+        )
+        self.tempo_to_batch = Rearrange("b t s d -> (b t) s d")
+
+        self.tempo_cls_token = nn.Parameter(torch.randn((1, 1, self.hidden_dim)))
+        self.prepend_tempo_cls_token = lambda x: torch.cat(
+            (self.tempo_cls_token.repeat(x.size(0), 1, 1), x), dim=1,
+        )
+        self.tempo_pos_embed = nn.Parameter(
+            torch.randn((1, self.tempo_seq_len, self.hidden_dim))
+        )
+        self.tempo_cls_pos_embed = nn.Parameter(
+            torch.randn((1, 1, self.hidden_dim)),
+        )
+
+        self.spat_encoder = self.vit_blocks
+        self.tempo_encoder = nn.Sequential(
+            *([self.spat_encoder[0]] * len(self.spat_encoder)),
+        )
+
     def forward(self, x):
         batch_size = x.size(0)
-        x = self.tuplet_embed(x)
+        x = self.tublet_embed(x)
 
-        x = x.contiguous() # (batch_size, tempo, spat, hidden_dim)
+        x = self.tempo_to_batch(x) # "$h_{i}$"
+        x = self.prepend_spat_cls_token(x)
+        x += torch.cat(
+            [self.spat_cls_pos_embed, self.spat_pos_embed], dim=1,
+        )
+        x = self.spat_encoder(x)
+        x = self.pool(x)
 
-        if self.mode == "spat_tempo_attn":
-            x = self.merge(x)
-            x = self.prepend_cls_token(x)
-            x += self.pos_embed
-            x = vit.blocks(x)
-            x = self.unmerge(x)
-            x = self.pool(x)
+        x = self.batch_to_tempo(x, batch_size=batch_size)
+        x = self.prepend_tempo_cls_token(x)
+        x += torch.cat(
+            [self.tempo_cls_pos_embed, self.tempo_pos_embed], dim=1,
+        )
+        x = self.tempo_encoder(x) # "$H$"
+        x = self.pool(x)
+        return self.mlp_head(x)
 
-        elif self.mode == "factor_encoder":
-            x = self.fold_tempo(x) # "$h_{i}$"
-            x = self.spat_encoder(x)
-            x = self.pool(x)
-            x = self.unfold_tempo(x, batch_size=batch_size)
-            x = self.tempo_encoder(x) # "$H$"
-            x = self.pool(x)
 
-        elif self.mode == "factor_self_attn":
-            for spat_block, tempo_block in zip(self.spat_blocks, self.tempo_blocks):
-                x = self.fold_tempo(x)
-                x = x + spat_block.drop_path1(
-                    spat_block.ls1(spat_block.attn(spat_block.norm1(x)))
-                )
-                x = self.unfold_tempo(x, batch_size=batch_size)
+class FactorSelfAttnViViT(ViViTBase):
+    """
+    "We factorise the operation to first only compute self-attention spatially (among all tokens extracted from the same tem- poral index), and then temporally (among all tokens ex- tracted from the same spatial index)"
+    "We do not use a classification token in this model, to avoid
+    ambiguities when reshaping the input tokens between spatial and
+    temporal dimensions.
+    """
+    def init_tempo_blocks(self):
+        """
+        "It contains two multi-headed self attention (MSA) modules. We
+        initialise the spatial MSA module from the pretrained module, and
+        initialise all weights of the temporal MSA with zeroes, such that Eq. 5
+        behaves as a residual connection at initialisation."
+        """
+        for layer in self.tempo_blocks:
+            for param in layer.parameters():
+                nn.init.constant_(param, 0)
 
-                x = self.fold_spat(x)
-                # temp = x
-                x = x + tempo_block(x)[0]
-                # print(torch.equal(x, temp))
-                x = self.unfold_spat(x, batch_size=batch_size)
+    def __init__(
+        self,
+        vit,
+        num_frames,
+        img_size,
+        tempo_patch_size,
+        spat_patch_size,
+        pooling_mode="cls",
+        num_classes=100,
+    ):
+        super().__init__(
+            vit=vit,
+            num_frames=num_frames,
+            img_size=img_size,
+            tempo_patch_size=tempo_patch_size,
+            spat_patch_size=spat_patch_size,
+            pooling_mode=pooling_mode,
+            num_classes=num_classes,
+        )
 
-                x = x + spat_block.drop_path2(
-                    spat_block.ls2(spat_block.mlp(spat_block.norm2(x)))
-                )
-            x = self.pool(x)
+        self.tempo_to_batch = Rearrange("b t s d -> (b t) s d")
+        self.spat_to_batch = Rearrange("b t s d -> (b s) t d")
+        self.spat_blocks = self.vit_blocks
+        self.tempo_blocks = nn.Sequential(
+            *(
+                [
+                    MSA(
+                        hidden_dim=self.hidden_dim,
+                        num_heads=num_heads,
+                        drop_prob=drop_prob,
+                    ),
+                    nn.LayerNorm(self.hidden_dim),
+                ] * len(self.spat_blocks)
+            )
+        )
+        self.init_tempo_blocks()
+        self.pooling_mode == "avg"
 
-        # x = self.mlp_head(x)
-        return x
+    def forward(self, x):
+        batch_size = x.size(0)
+        x = self.tublet_embed(x)
+
+        for spat_block, tempo_block in zip(self.spat_blocks, self.tempo_blocks):
+            x = self.tempo_to_batch(x)
+            x = x + spat_block.drop_path1(
+                spat_block.ls1(spat_block.attn(spat_block.norm1(x)))
+            )
+            x = self.untempo_to_batch(x, batch_size=batch_size)
+
+            x = self.spat_to_batch(x)
+            # temp = x
+            x = x + tempo_block(x)[0]
+            # print(torch.equal(x, temp))
+            x = self.unspat_to_batch(x, batch_size=batch_size)
+
+            x = x + spat_block.drop_path2(
+                spat_block.ls2(spat_block.mlp(spat_block.norm2(x)))
+            )
+        x = self.pool(x)
+        return self.mlp_head(x)
 
 
 if __name__ == "__main__":
+    num_frames = 8
     img_size = 64
-    vid_len = 16
+    tempo_patch_size=2
+    spat_patch_size=16
 
     device = torch.device("cuda")
     vit = timm.create_model("vit_base_patch16_224", pretrained=True).to(device)
-    # model = ViViT(vit=vit, pooling_mode="cls", mode="spat_tempo_attn").to(device)
-    # model = ViViT(vit=vit, mode="factor_encoder").to(device)
-    model = ViViT(vit=vit, mode="factor_self_attn").to(device)
-    video = torch.randn((4, 3, vid_len, img_size, img_size)).to(device)
+    # model = SpatTempoAttnViViT(
+    model = FactorEncViViT(
+        vit=vit,
+        num_frames=num_frames,
+        img_size=img_size,
+        tempo_patch_size=tempo_patch_size,
+        spat_patch_size=spat_patch_size,
+        pooling_mode="cls",
+    ).to(device)
+    
+    video = torch.randn((4, 3, num_frames, img_size, img_size)).to(device)
     x = model(video)
     x.shape
